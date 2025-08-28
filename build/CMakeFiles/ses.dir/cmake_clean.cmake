27262ab0adc33349de343117f0251c74f6f4c6fb
Utku-Ogul <utkuogulbolat250@gmail.com>
2025-08-28 09:58:49 +0300

    loopback fix

---
+++ build/CMakeFiles/ses.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ses.dir/alsa.c.o"
+  "CMakeFiles/ses.dir/alsa.c.o.d"
+  "CMakeFiles/ses.dir/main.c.o"
+  "CMakeFiles/ses.dir/main.c.o.d"
+  "CMakeFiles/ses.dir/udp.c.o"
+  "CMakeFiles/ses.dir/udp.c.o.d"
+  "CMakeFiles/ses.dir/udp_codec.c.o"
+  "CMakeFiles/ses.dir/udp_codec.c.o.d"
+  "ses"
+  "ses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ses.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ses.
+# This may be replaced when dependencies are built.
